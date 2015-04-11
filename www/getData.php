cc0afe13e0c6aaccbe22381181c4938e8a0d27a4
Greg <pickernickel1@gmail.com>
2015-04-11 05:26:19 -0500

    Implemented Blob Query
    getData.php returns the blobs in range given the appropriate arguments.

--- www/getData.php
+++ www/getData.php
@@ -2,5 +2,23 @@
     include_once 'session.php';
     include_once 'sql.php';
     
-    echo "This page is meh.";
+    $lat = $_GET['lat'];
+    $long = $_GET['long'];
+    $radius = $_GET['radius'];
+    
+    $llat = $lat - $radius;
+    $ulat = $lat + $radius;
+    $llong = $long - $radius;
+    $ulong = $long + $radius;
+    
+    $conn['queryBlobs']->bind_param("dddd", $llat, $ulat, $llong, $ulong);
+    $conn['queryBlobs']->execute();
+    $conn['queryBlobs']->bind_result($id,$lat,$long,$user,$amount,$charityid);
+    
+    $result = array();
+    while($conn['queryBlobs']->fetch())
+    {
+        array_push($result, array($id,$lat,$long,$user,$amount,$charityid));
+    }
+    echo json_encode($result);
 ?>
--- www/sql.php
+++ www/sql.php
@@ -6,9 +6,30 @@
 	$pass = "root";
 	$database = "charity";
     
-    $conn = new mysqli($host, $username, $pass, $database);
-    if($conn->connect_error)
-	{
-		die();
-	}
+    function getConn()
+    {
+        static $conn;
+        if(!$conn)
+        {
+            $host = "localhost";
+            $username = "root";
+            $pass = "root";
+            $database = "charity";
+            
+            $conn['conn'] = new mysqli("p:".$host, $username, $pass, $database);
+            if(mysqli_connect_errno())
+            {
+                die();
+            }
+            $conn['queryBlobs'] = $conn['conn']->prepare("SELECT * FROM blobs
+                                                            WHERE (latitude BETWEEN ? AND ?)
+                                                            AND (longitude BETWEEN ? AND ?);");
+            echo htmlspecialchars($conn['conn']->error);
+            $conn['queryCharities'] = $conn['conn']->prepare("SELECT `charities`.`id`,`charities`.`name`,`charities`.`color` FROM `charity`.`charities`;");
+            $conn['prepBlob'] = $conn['conn']->prepare("");
+        }
+        return $conn;
+    }
+    
+    $conn = getConn();
 ?>
